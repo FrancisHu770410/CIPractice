34bb90a4858b7ba1b729f356a7095d389222fa2f
Francis <FrancisHu770410@gmail.com>
2017-03-07 14:05:17 +0800

    106/03/07 MainViewController

--- CIPractice/AppDelegate.m
+++ CIPractice/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MainController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,10 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    MainController *mainController = [[MainController alloc] init];
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.rootViewController = mainController;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
